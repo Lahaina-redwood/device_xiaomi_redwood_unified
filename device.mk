95e0fb8fc622d85047cf68821f635776c286b157
AdarshGrewal <adarshgrewal@gmail.com>
2023-10-13 14:18:37 +0300

    redwood: Switch to AOSP WFD
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- device.mk
+++ device.mk
@@ -622,14 +622,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    android.media.audio.common.types-V2-cpp \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_PACKAGES += \
     libpng.vendor
